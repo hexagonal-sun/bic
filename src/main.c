70f436fbafe2e7c82c337f478737ca21e9408302
Matthew Leach <matthew@mattleach.net>
2015-07-16 14:22:03 +0100

    main: implement a REPL
    Add a very basic REPL, using readline to get the string passed to the
    program and passing that to the parser.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
+#include "config.h"
 #include "tree.h"
 #include "parser.h"
 #include "lex.h"
 #include <stdio.h>
+#ifdef HAVE_LIBREADLINE
+#  if defined(HAVE_READLINE_READLINE_H)
+#    include <readline/readline.h>
+#  elif defined(HAVE_READLINE_H)
+#    include <readline.h>
+#  else /* !defined(HAVE_READLINE_H) */
+extern char *readline ();
+#  endif /* !defined(HAVE_READLINE_H) */
+char *cmdline = NULL;
+#else /* !defined(HAVE_READLINE_READLINE_H) */
+#error "No readline found"
+#endif /* HAVE_LIBREADLINE */
+
 
 extern FILE* yyin;
 
@@ -17,6 +31,12 @@ void yyerror(const char *str)
 
 int main()
 {
-    yyin = stdin;
-    yyparse();
+    char *line = readline(BIC_PROMPT);
+    while (line) {
+        YY_BUFFER_STATE buffer = yy_scan_string(line);
+        yyparse();
+        yy_delete_buffer(buffer);
+        tree_dump(parse_head);
+        line = readline(BIC_PROMPT);
+    }
 }
