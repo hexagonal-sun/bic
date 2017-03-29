74c17b12b3c8f111fe5d9a761feea327faf3ed05
Matthew Leach <matthew@mattleach.net>
2017-03-29 07:52:23 +0100

    x86_64: new
    Add a new set of functions and files, native to x86_64 for handling
    external function calls on that platform.

---
+++ src/x86_64/function_call.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "function_call.h"
+
+struct arg *arg_head = NULL;
+extern void __do_call(void *fn, struct arg *args);
+
+
+/* Here we simplify the interface for accessing arguments so that
+ * it is reasonable to be done in assembly. */
+void do_call(void *function_address, tree args)
+{
+    tree arg;
+
+    if (!args)
+        return;
+
+    for_each_tree(arg, args) {
+        struct arg *new_arg = malloc(sizeof(*new_arg));
+        new_arg->next = arg_head;
+        switch(arg->type) {
+        case T_STRING:
+            new_arg->val = (ptrdiff_t)strdup(arg->data.string);
+            new_arg->class = POINTER;
+            break;
+        default:
+            fprintf(stderr, "Error: Unknown tree type to marshall.\n");
+            exit(1);
+        }
+        arg_head = new_arg;
+    }
+
+    __do_call(function_address, arg_head);
+
+    /* Cleanup consumed arguments. */
+    while (arg_head) {
+        struct arg *old_arg = arg_head;
+        arg_head = arg_head->next;
+        free(old_arg);
+    }
+}
---
+++ src/x86_64/function_call.h
@@ -0,0 +1,23 @@
+#include "../tree.h"
+
+enum argument_class {
+    POINTER,
+    INTEGER,
+    SSE,
+    SSEUP,
+    X87,
+    X87UP,
+    COMPLEX_X87,
+    NO_CLASS,
+    MEMORY,
+};
+
+/* Ensure no structure packing occurs, since we'll be accessing the
+ * structure from assembly. */
+struct arg {
+    struct arg *next;
+    ptrdiff_t val;
+    enum argument_class class;
+} __attribute__((packed)) ;
+
+void do_call(void *function_address, tree args);
