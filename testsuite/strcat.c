ef9c3bd8611807fcd3b0b3683d87122484938340
Matthew Leach <matthew@mattleach.net>
2017-03-31 20:48:18 +0100

    testsuite: strcat: new
    Add a new test that uses multiple string manipulation functions to
    test multi-argument function calls.

---
+++ testsuite/strcat.c
@@ -0,0 +1,19 @@
+char *malloc(int len);
+int strlen(char *s);
+int puts(char *s);
+char *strcpy(char *dest, char *src);
+char *strcat(char *s1, char *s2);
+
+int main()
+{
+    char *s1, *s2, *s3;
+
+    s1 = "Hello, ";
+    s2 = "World!";
+    s3 = malloc(strlen(s1) + strlen(s2) + 2);
+    puts(s1);
+    puts(s2);
+    strcpy(s3, s1);
+    strcat(s3, s2);
+    puts(s3);
+}
