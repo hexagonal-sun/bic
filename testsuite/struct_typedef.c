e7d42306aa7228db656bfb4530bcde3d586c9570
Matthew Leach <matthew@mattleach.net>
2017-04-05 21:26:04 +0100

    testsuite: simple_typedef: new
    Add a new test for ensuring structure typedefs work correctly.

---
+++ testsuite/struct_typedef.c
@@ -0,0 +1,15 @@
+int printf(char *s, int d);
+
+typedef struct {
+    int a;
+    int b;
+} bana;
+
+int main()
+{
+    bana baz;
+    baz.a = 10;
+    baz.b = 1249;
+    printf("%d\n", baz.a);
+    printf("%d\n", baz.b);
+}
