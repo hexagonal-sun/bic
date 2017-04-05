40f314a05a975dbb97dd84879843cec004a8221c
Matthew Leach <matthew@mattleach.net>
2017-04-05 21:32:16 +0100

    testsuite: add: new
    Add a new test program to ensure addition functions work correctly.

---
+++ testsuite/add.c
@@ -0,0 +1,12 @@
+int printf(char *s, int d);
+
+int main()
+{
+    int a = 1024;
+    int b = 345875;
+
+    printf("%d\n", a + 3587);
+    printf("%d\n", 234587 + 3553);
+    printf("%d\n", 11212 + b);
+    printf("%d\n", a + b);
+}
