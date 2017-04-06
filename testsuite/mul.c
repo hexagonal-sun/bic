67c6ce40d0f15c9e3c231f4d17c3f674a2f4e969
Matthew Leach <matthew@mattleach.net>
2017-04-06 19:58:19 +0100

    testsuite: mul: new
    Add a new test case for multiplications.

---
+++ testsuite/mul.c
@@ -0,0 +1,14 @@
+int printf(char *s, int d);
+
+int main()
+{
+    int a = 1024;
+    int b = 24;
+    unsigned int c = 0;
+
+    printf("%d\n", a * 3587);
+    printf("%d\n", 234587 * 18);
+    printf("%d\n", 11212 * b);
+    printf("%d\n", a * b);
+    printf("%u\n", c * a);
+}
