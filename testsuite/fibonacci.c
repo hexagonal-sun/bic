c301fe39506621ebbb90c2172dcec2ec767764c7
Matthew Leach <matthew@mattleach.net>
2017-04-14 18:40:52 +0100

    testsuite: fibonacci: new
    Add a new testcase that makes use of a for loop for fibonacci
    calculation.

---
+++ testsuite/fibonacci.c
@@ -0,0 +1,17 @@
+int printf(char *s, ...);
+
+int a1 = 1;
+int a2 = 1;
+
+int main()
+{
+    int i;
+
+    for (i = 0; i < 20; i++) {
+        int next;
+        next = a1 + a2;
+        a1 = a2;
+        a2 = next;
+        printf("%d\n", next);
+    }
+}
