0bd493fe9e1307efcca2207debd9afd89d65662d
Matthew Leach <matthew@mattleach.net>
2017-04-14 14:44:56 +0100

    testsuite: gteq: new
    Add a new testcase for ensuring the '>=' operator works correctly.

---
+++ testsuite/gteq.c
@@ -0,0 +1,21 @@
+int printf(char *s, ...);
+
+int main()
+{
+    int a, b, c;
+
+    a = 24;
+    b = 45;
+    c = -241;
+
+    printf("%d\n", 2 >= 3);
+    printf("%d\n", 2 >= 2);
+    printf("%d\n", a >= b);
+    printf("%d\n", b >= a);
+    printf("%d\n", c >= a);
+    printf("%d\n", c >= b);
+    printf("%d\n", b >= c);
+    printf("%d\n", a >= c);
+    printf("%d\n", c >= -242);
+    printf("%d\n", a >= 25);
+}
