8f132e11b5fe852c1571ea1dc5e7e28ab75fd9ef
Matthew Leach <matthew@mattleach.net>
2017-04-16 17:21:07 +0100

    testsuite: linked-list: new
    Add a new test that checks that structure references can be created by
    calls to malloc as well as having a member of a structure that is a
    pointer to itself.

---
+++ testsuite/linked-list.c
@@ -0,0 +1,42 @@
+void free(void *ptr);
+void *malloc(int len);
+int printf(char *s, ...);
+
+typedef int footype;
+
+struct list {
+    struct list *next;
+    footype val;
+};
+
+struct list *head;
+
+void push_val(int val)
+{
+    struct list *new;
+
+    new = malloc(sizeof(struct list));
+
+    new->next = head;
+    new->val = val;
+    head = new;
+}
+
+int main()
+{
+    int i;
+
+    head = 0;
+
+    for (i = 0; i < 1000; i++)
+        push_val(i);
+
+    printf("%d\n", head->next->next->next->next->val);
+
+    for (i = 0; i < 1000; i++) {
+        struct list *tmp;
+        tmp = head->next;
+        free(head);
+        head = tmp;
+    }
+}
