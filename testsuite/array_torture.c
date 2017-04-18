bbc02468a5b2803ba058a1d612a088a71eebc364
Matthew Leach <matthew@mattleach.net>
2017-04-18 06:54:06 +0100

    testsuite: array_torture: new
    Add a new test case for stretching the array invocation code.

---
+++ testsuite/array_torture.c
@@ -0,0 +1,40 @@
+int printf(char *fmt, ...);
+
+struct foo {
+    int a;
+    int b;
+    int bazza[15];
+};
+
+void print_foo(struct foo *p)
+{
+    int i;
+
+    for (i = 0; i < 15; i++)
+        printf("%d %d %d\n", p->a, p->b, p->bazza[i]);
+}
+
+int main()
+{
+    int i;
+    long int double_array[20];
+    struct foo ff[20];
+
+    for (i = 0; i < 20; i++) {
+        int j;
+        double_array[i] = i * 10;
+
+        ff[i].a = i;
+        ff[i].b = i + 1;
+
+        for (j = 0; j < 15; j++) {
+            ff[i].bazza[j] = j + i;
+        }
+    }
+
+    for (i = 0; i < 20; i++)
+        printf("%d\n", double_array[i]);
+
+    for (i = 0; i < 20; i++)
+        print_foo(&ff[i]);
+}
