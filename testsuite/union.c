be5049e9bf2121190f037c0f800e4b9b0623ec5e
Matthew Leach <matthew@mattleach.net>
2017-04-18 17:46:40 +0100

    testsuite: union: new
    Add a new test case for checking unions.

---
+++ testsuite/union.c
@@ -0,0 +1,22 @@
+void free(void *ptr);
+void *malloc(int length);
+int printf(char *s, ...);
+
+union foobar {
+    int a;
+    float b;
+    void *ptr;
+    unsigned char array[4];
+};
+
+int main()
+{
+    union foobar foob;
+
+    foob.ptr = 0;
+    foob.a = 0;
+    foob.b = 2.42;
+    printf("%d %f %p\n", foob.a, foob.b, foob.ptr);
+    printf("%u %u %u %u\n", foob.array[0], foob.array[1],
+           foob.array[2], foob.array[3]);
+}
