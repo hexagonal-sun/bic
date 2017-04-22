78ec496836e7b27e83345365fcca4ad66d8e0bc9
Matthew Leach <matthew@mattleach.net>
2017-04-22 15:29:07 +0100

    testsuite: forward_declare: new
    Add a test case for checking forward-declaration code.

---
+++ testsuite/forward_declare.c
@@ -0,0 +1,49 @@
+int printf(char *s, ...);
+void *malloc(int len);
+void free(void *ptr);
+
+struct foobar;
+
+typedef struct foobar foostruct;
+
+struct baz {
+    struct foobar *fab;
+};
+
+struct a {
+    foostruct *fooelm;
+};
+
+int other()
+{
+    struct foobar {
+        float d;
+    } ff;
+
+    ff.d = 20;
+
+    printf("%f\n", ff.d);
+}
+
+struct foobar {
+    int a;
+};
+
+int main()
+{
+    struct baz bag;
+    struct a bat;
+
+    bag.fab = malloc(sizeof(struct foobar));
+    bat.fooelm = malloc(sizeof(foostruct));
+
+    other();
+
+    bag.fab->a = 10;
+    bat.fooelm->a = 1024;
+
+    printf("%d\n", bag.fab->a);
+    printf("%d\n", bat.fooelm->a);
+
+    free(bag.fab);
+}
