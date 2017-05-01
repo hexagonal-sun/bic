1132065db4c38861e919c74bab6e161bfe50fbb8
Matthew Leach <matthew@mattleach.net>
2017-05-01 11:41:39 +0100

    testsuite: double_pointer: new
    Add a new testcase for checking double pointer semantics.

---
+++ testsuite/double_pointer.c
@@ -0,0 +1,33 @@
+int printf(char *s, ...);
+
+int foobar;
+
+void set_ptr_to_foobar(int **pptr)
+{
+    *pptr = &foobar;
+}
+
+int main()
+{
+    int a;
+    int *iptr;
+
+    a = 10;
+    foobar = 20;
+
+    printf("%d\n", a);
+    printf("%d\n", foobar);
+
+    iptr = &a;
+    *iptr = 2020;
+
+    printf("%d\n", a);
+    printf("%d\n", foobar);
+
+    set_ptr_to_foobar(&iptr);
+    *iptr = 2020;
+
+    printf("%d\n", a);
+    printf("%d\n", foobar);
+
+}
