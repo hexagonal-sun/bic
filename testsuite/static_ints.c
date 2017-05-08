4c7cd1b9d49f3f07774a6ff376ea41476c3c9bb9
Matthew Leach <matthew@mattleach.net>
2017-05-08 21:39:43 +0100

    testsuite: static_ints: new
    Add a new testcase to ensure static integers work correctly.

---
+++ testsuite/static_ints.c
@@ -0,0 +1,31 @@
+int printf(char *s, ...);
+
+int a = 2424;
+
+void foofunc()
+{
+    int i;
+    static int a = 10, b = 20;
+
+    for (i = 0; i < 5; i++)
+    {
+        static int a = 4424;
+        a++;
+
+        printf("%d\n", a);
+    }
+
+    b--;
+
+    printf("%d\n", a++);
+    printf("%d\n", b);
+}
+
+int main ()
+{
+    foofunc();
+    foofunc();
+    foofunc();
+
+    printf("%d\n", a);
+}
