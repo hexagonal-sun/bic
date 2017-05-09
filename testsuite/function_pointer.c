b7e4f22483e2e12f63dc760bc9f3781452a5871a
Matthew Leach <matthew@mattleach.net>
2017-05-09 22:09:31 +0100

    testsuite: function_pointer: add function pointer decl
    Add a function pointer declaration to the function_pointer test case,
    ensuring that they are parsed and work correctly.

--- testsuite/function_pointer.c
+++ testsuite/function_pointer.c
@@ -16,19 +16,39 @@ void doSub(int ia, int ib)
     b = b - ia;
 }
 
+void add_ptr_func(int *a)
+{
+    (*a)++;
+}
+
+void sub_ptr_func(int *b)
+{
+    (*b)--;
+}
+
 int main ()
 {
     ftype *func;
 
+    void (* fptr)(int *p);
+
     a = 10;
     b = 20;
+    fptr = &add_ptr_func;
+    fptr(&a);
+    fptr(&b);
     printf("%d %d\n", a, b);
 
     func = &doAdd;
     func(a, b);
+    fptr(&a);
+    fptr(&a);
     printf("%d %d\n", a, b);
 
     func = &doSub;
+    fptr = &sub_ptr_func;
+    fptr(&a);
+    fptr(&b);
     func(a, b);
 
     printf("%d %d\n", a, b);
