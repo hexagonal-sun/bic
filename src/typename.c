8fbdf22f78dda840ee090b95ebdd297db3980bde
Matthew Leach <matthew@mattleach.net>
2017-08-19 10:39:49 +0100

    typename: track #include typenames
    Allow typenames that are seen in #include file to be tracked separately
    as well as being reset.

--- src/typename.c
+++ src/typename.c
@@ -3,14 +3,36 @@
 
 static tree type_names;
 
+/* Typenames that are located within #include files. */
+static tree include_type_names;
+static int in_include_file = 0;
+
 void add_typename(char *s)
 {
-    tree_chain(get_identifier(s), type_names);
+    tree_chain(get_identifier(s),
+               in_include_file ? include_type_names : type_names);
+}
+
+void typename_set_include_file(void)
+{
+    in_include_file = 1;
+}
+
+void typename_unset_include_file(void)
+{
+    in_include_file = 0;
+}
+
+void reset_include_typenames(void)
+{
+    include_type_names = tree_make(CHAIN_HEAD);
 }
 
 void typename_init(void)
 {
     type_names = tree_make(CHAIN_HEAD);
+
+    reset_include_typenames();
 
     /* Add all builtin types here so the parser knows about them. */
     add_typename("__builtin_va_list");
@@ -21,6 +43,11 @@ int is_typename(char *identifier)
     tree i;
 
     for_each_tree(i, type_names) {
+        if (strcmp(identifier, tID_STR(i)) == 0)
+            return 1;
+    }
+
+    for_each_tree(i, include_type_names) {
         if (strcmp(identifier, tID_STR(i)) == 0)
             return 1;
     }
--- src/typename.h
+++ src/typename.h
@@ -1,5 +1,8 @@
 #include "tree.h"
 
+void typename_set_include_file(void);
+void typename_unset_include_file(void);
+void reset_include_typenames(void);
 void add_typename(char *s);
 void typename_init(void);
 int is_typename(char *identifier);
