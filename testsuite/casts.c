275d420c7667b9d5d64f47bd0d16c66c0174c75f
Matthew Leach <matthew@mattleach.net>
2017-11-03 19:41:30 +0000

    testsuite: casts: new
    Add a testcast for casting expressions.

---
+++ testsuite/casts.c
@@ -0,0 +1,57 @@
+int printf(char *s, ...);
+
+int main()
+{
+    int a = 1024;
+    int b = 345875;
+    unsigned long c = 20202042;
+    int d = -2424;
+    float e = 24.124;
+
+    printf("%d\n", a);
+    printf("%d\n", (char)a);
+    printf("%u\n", (unsigned char)a);
+    printf("%ld\n", (long)a);
+    printf("%lu\n",(unsigned long)a);
+    printf("%d\n", (int)a);
+    printf("%u\n", (unsigned int)a);
+    printf("%f\n", (float)a);
+
+    printf("%d\n", b);
+    printf("%d\n", (char)b);
+    printf("%u\n", (unsigned char)b);
+    printf("%ld\n", (long)b);
+    printf("%lu\n",(unsigned long)b);
+    printf("%d\n", (int)b);
+    printf("%u\n", (unsigned int)b);
+    printf("%f\n", (float)b);
+
+    printf("%lu\n", c);
+    printf("%d\n", (char)c);
+    printf("%u\n", (unsigned char)c);
+    printf("%ld\n", (long)c);
+    printf("%lu\n",(unsigned long)c);
+    printf("%d\n", (int)c);
+    printf("%u\n", (unsigned int)c);
+    printf("%f\n", (float)c);
+
+    printf("%d\n", d);
+    printf("%d\n", (char)d);
+    printf("%u\n", (unsigned char)d);
+    printf("%ld\n", (long)d);
+    printf("%lu\n",(unsigned long)d);
+    printf("%d\n", (int)d);
+    printf("%u\n", (unsigned int)d);
+    printf("%f\n", (float)d);
+
+    printf("%f\n", e);
+    printf("%d\n", (char)e);
+    printf("%u\n", (unsigned char)e);
+    printf("%ld\n", (long)e);
+    printf("%lu\n",(unsigned long)e);
+    printf("%d\n", (int)e);
+    printf("%u\n", (unsigned int)e);
+    printf("%f\n", (float)e);
+
+
+}
