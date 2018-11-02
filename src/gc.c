2c706dc2679e9e80088d87248235c47ed7127e16
Matthew Leach <matthew@mattleach.net>
2018-11-02 18:35:41 +0000

    gc: Remove mark function
    Remove the mark function and disable GC for now as this will be
    generated with gengc.

--- src/gc.c
+++ src/gc.c
@@ -21,138 +21,7 @@ static void mark_tree(tree obj)
 
 static void mark_object(gc_obj obj)
 {
-    tree t;
-
-    if (!obj)
-        return;
-
-    obj->reachable = 1;
-
-    t = &obj->t;
-
-    switch (TYPE(t)) {
-    case T_P_INC:
-    case T_P_DEC:
-    case T_INC:
-    case T_DEC:
-    case T_SIZEOF:
-    case T_FN_ARG:
-    case T_POINTER:
-    case T_ADDR:
-    case T_DEREF:
-    case T_EXTERN:
-    case T_STATIC:
-    case T_RETURN:
-    case T_STRUCT:
-    case T_UNION:
-    case T_TYPEDEF:
-    case D_T_PTR:
-        mark_tree(t->data.exp);
-        break;
-    case T_MUL:
-    case T_DIV:
-    case T_MOD:
-    case T_ADD:
-    case T_SUB:
-    case T_LSHIFT:
-    case T_RSHIFT:
-    case T_LT:
-    case T_GT:
-    case T_LTEQ:
-    case T_GTEQ:
-    case T_EQ:
-    case T_CAST:
-    case T_ARRAY:
-    case T_ARRAY_TYPE:
-    case T_N_EQ:
-    case T_L_OR:
-    case T_L_AND:
-    case T_ARRAY_ACCESS:
-    case T_COMP_ACCESS:
-    case T_ASSIGN:
-    case E_MAP:
-        mark_tree(t->data.bin.left);
-        mark_tree(t->data.bin.right);
-        break;
-    case T_DECL:
-        mark_tree(t->data.decl.type);
-        mark_tree(t->data.decl.decls);
-        break;
-    case T_DECL_COMPOUND:
-        mark_tree(t->data.comp_decl.id);
-        mark_tree(t->data.comp_decl.decls);
-        break;
-    case T_DECL_FN:
-    case T_FN_DEF:
-        mark_tree(t->data.function.id);
-        mark_tree(t->data.function.return_type);
-        mark_tree(t->data.function.arguments);
-        mark_tree(t->data.function.stmts);
-        break;
-    case T_LOOP_FOR:
-        mark_tree(t->data.floop.initialization);
-        mark_tree(t->data.floop.condition);
-        mark_tree(t->data.floop.afterthrought);
-        mark_tree(t->data.floop.stmts);
-        break;
-    case T_FN_CALL:
-        mark_tree(t->data.fncall.identifier);
-        mark_tree(t->data.fncall.arguments);
-        break;
-    case T_LIVE_VAR:
-        mark_tree(t->data.var.type);
-        break;
-    case CHAIN_HEAD:
-            {
-                tree i;
-                for_each_tree(i, t)
-                    mark_tree(i);
-            }
-            break;
-    case E_CTX:
-        mark_tree(t->data.ectx.id_map);
-        mark_tree(t->data.ectx.parent_ctx);
-        mark_tree(t->data.ectx.alloc_chain);
-        break;
-    case T_LIVE_COMPOUND:
-        mark_tree(t->data.comp.members);
-        mark_tree(t->data.comp.decl);
-        break;
-    case T_ENUMERATOR:
-        mark_tree(t->data.enumerator.id);
-        mark_tree(t->data.enumerator.enums);
-        break;
-    case T_IF:
-        mark_tree(t->data.ifstmt.condition);
-        mark_tree(t->data.ifstmt.true_stmts);
-        mark_tree(t->data.ifstmt.else_stmts);
-        break;
-    case T_INTEGER:
-    case T_FLOAT:
-    case T_STRING:
-    case T_IDENTIFIER:
-    case T_VARIADIC:
-    case E_ALLOC:
-    case E_INCOMP_TYPE:
-    case CPP_INCLUDE:
-    case D_T_VOID:
-    case D_T_CHAR:
-    case D_T_SHORT:
-    case D_T_INT:
-    case D_T_LONG:
-    case D_T_LONGLONG:
-    case D_T_UCHAR:
-    case D_T_USHORT:
-    case D_T_UINT:
-    case D_T_ULONG:
-    case D_T_ULONGLONG:
-    case D_T_FLOAT:
-    case D_T_DOUBLE:
-    case D_T_LONGDOUBLE:
-        /* All other types don't need to recurse as they don't contain
-         * any other objects. */
-        break;
-    }
+    return;
 }
 
 static int compare_allocs(const void *a1, const void *a2)
@@ -286,6 +155,8 @@ static void collect(void)
 static void maybe_collect()
 {
     static int collect_counter = 0;
+
+    return;
 
     if (gc_inhibited)
         return;
