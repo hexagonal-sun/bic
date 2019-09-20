8afcca60cdcb75dcb63f013e73e498af346a6821
Matthew Leach <matthew@mattleach.net>
2019-09-20 18:04:07 +0100

    [evaluate] fix tree type missmatch
    The tree checker was failing as we where accessing tFNDECL_ARGS on a T_LIVE_VAR.
    This should have been the variable `function_type' which is the T_FN_DECL for
    the live var.

--- src/evaluate.c
+++ src/evaluate.c
@@ -505,7 +505,7 @@ static tree eval_fn_call(tree t, int depth)
         if (!is_T_DECL_FN(function_type))
             eval_die(t, "could not call non-function pointer type\n");
 
-        for_each_tree(i, tFNDECL_ARGS(function))
+        for_each_tree(i, tFNDECL_ARGS(function_type))
             if (is_T_VARIADIC(i)){
                 is_variadic = true;
                 break;
