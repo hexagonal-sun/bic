3e4e56e98c668212348d9ad33966e3f1d2594fe8
Matthew Leach <matthew@mattleach.net>
2021-02-07 19:55:15 +0000

    [function-call] respect the 'default argument promotions'
    When calling varadic functions we need to respect the 'default argument
    promotions' and ensure that floats are promoted to double values.

--- src/function-call.c
+++ src/function-call.c
@@ -6,13 +6,33 @@
 
 #include "ffi-types.h"
 #include "function-call.h"
+#include "evaluate.h"
+#include "gc.h"
 
 static ffi_type *type_stack[32];
 static void *val_stack[32];
 static int n = 0;
 
-static void push_arg(ffi_type *type, void* val)
+static void push_arg(ffi_type *type, void* val, bool should_promote)
 {
+    /* Acording to the 'default argument promotions' floats should be promotoed
+       to doubles and <int should be promoted to int when being passed as a
+       varadic parameter. */
+#define PROMOTE(FFI_TYPE, CTYPE, TARGET_T_TYPE, TARGET_C_TYPE, TARGET_FFI_TYPE) \
+    if (should_promote && type == &(FFI_TYPE)) {                        \
+        tree dlv = make_live_var(tree_make(TARGET_T_TYPE));             \
+        CTYPE *cval = (CTYPE *)val;                                     \
+        tLV_VAL(dlv)->TARGET_T_TYPE = *cval;                            \
+        val = &tLV_VAL(dlv)->TARGET_T_TYPE;                             \
+        type = &(TARGET_FFI_TYPE);                                      \
+    }
+    PROMOTE(ffi_type_float, float, D_T_DOUBLE, double, ffi_type_double);
+    PROMOTE(ffi_type_schar, char, D_T_INT, int, ffi_type_sint);
+    PROMOTE(ffi_type_sshort, short, D_T_INT, int, ffi_type_sint);
+    PROMOTE(ffi_type_uchar, unsigned char, D_T_UINT, unsigned int, ffi_type_uint);
+    PROMOTE(ffi_type_ushort, unsigned short, D_T_UINT, unsigned int, ffi_type_uint);
+#undef PROMOTE
+
     if (n == 32) {
         fprintf(stderr , "Max num args reached.\n");
         exit(1);
@@ -32,19 +52,26 @@ void do_ext_call(void *function_address, tree args, tree ret_lv,
     n = 0;
     ffi_type *ret_type = &ffi_type_void;
 
+    // Prevent GC here, we could potentially create temporary LV for the default
+    // argument promotions. FFI would have a reference to the LVs but the trees
+    // would be dangling, making them a candidate for GC. Prevent that until
+    // after function calling is complete.
+    inhibit_gc();
+
     if (args)
         for_each_tree(fn_arg, args) {
+            bool should_promote = variadic_pos ? n >= *variadic_pos : false;
             tree arg = tFNARG_EXP(fn_arg);
 
             switch(TYPE(arg)) {
             case T_STRING:
-                push_arg(&ffi_type_pointer, &tSTRING_VAL(arg));
+                push_arg(&ffi_type_pointer, &tSTRING_VAL(arg), should_promote);
                 break;
             case T_LIVE_VAR:
                 switch(TYPE(tLV_TYPE(arg))) {
 #define DEFCTYPE(TNAME, DESC, STDINTSZ, FMT, FFMEM)                     \
                     case TNAME:                                         \
-                        push_arg(get_ffi_type(TNAME), &tLV_VAL(arg)->TNAME); \
+                        push_arg(get_ffi_type(TNAME), &tLV_VAL(arg)->TNAME, should_promote); \
                         break;
 #include "ctypes.def"
 #undef DEFCTYPE
@@ -56,14 +83,14 @@ void do_ext_call(void *function_address, tree args, tree ret_lv,
                 {
                     int *buf = alloca(sizeof(int));
                     *buf = mpz_get_si(tINT_VAL(arg));
-                    push_arg(&ffi_type_sint, buf);
+                    push_arg(&ffi_type_sint, buf, should_promote);
                     break;
                 }
             case T_FLOAT:
                 {
                     double *buf = alloca(sizeof(double));
                     *buf = mpf_get_d(tFLOAT_VAL(arg));
-                    push_arg(&ffi_type_double, buf);
+                    push_arg(&ffi_type_double, buf, should_promote);
                     break;
                 }
             default:
@@ -81,4 +108,6 @@ void do_ext_call(void *function_address, tree args, tree ret_lv,
         ffi_prep_cif(&cif, FFI_DEFAULT_ABI, n, ret_type, &type_stack);
 
     ffi_call(&cif, function_address, ret_lv ? (void *)tLV_VAL(ret_lv) : NULL, val_stack);
+
+    enable_gc();
 }
