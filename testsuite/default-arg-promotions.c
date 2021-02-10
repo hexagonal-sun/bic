273b2d5974234cce3c29fcaf026afea7719f938e
Matthew Leach <matthew@mattleach.net>
2021-02-10 08:37:05 +0000

    [testsuite] default-arg-promotions: new
    Add a new testcase that ensures that the default argument promotions are
    implemented correctly.

---
+++ testsuite/default-arg-promotions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = -2;
+    unsigned char b = 2;
+    short c = -300;
+    unsigned short d = 300;
+    float f = 1.25484;
+
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", d);
+    printf("%f\n", f);
+
+    return 0;
+}
